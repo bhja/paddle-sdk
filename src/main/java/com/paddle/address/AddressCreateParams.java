b08d6305269433a0ddad952365638da3de728c90
Bhagawathi <bh.ja.699@gmail.com>
2024-02-01 22:12:51 +0530

    code reformatter

--- src/main/java/com/paddle/address/AddressCreateParams.java
+++ src/main/java/com/paddle/address/AddressCreateParams.java
@@ -2,13 +2,17 @@ package com.paddle.address;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class AddressCreateParams {
 
   @JsonProperty("first_line")
--- src/main/java/com/paddle/business/BusinessCreateParams.java
+++ src/main/java/com/paddle/business/BusinessCreateParams.java
@@ -4,9 +4,19 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.paddle.model.Contact;
 import java.util.List;
 import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BusinessCreateParams {
-
   private String name;
 
   @JsonProperty("company_number")
